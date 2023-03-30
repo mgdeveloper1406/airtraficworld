5c32b587a2cf1d0ecdd36720c3756d15e057eb3b
MG_Apps <99666081+mgdeveloper1406@users.noreply.github.com>
2023-03-30 15:44:28 -0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -6,7 +6,7 @@ function chillHop() {
         {
             name:"LSZH Apron (South)",
             artist: "Zurich, Switzerland",
-            cover: "https://cdn.xxl.thumbs.canstockphoto.com/illustration-of-radar-vector-clipart_csp5212180.jpg",
+            cover: "https://us.123rf.com/450wm/micelinjo/micelinjo1406/micelinjo140600004/28995816-pantalla-de-radar-con-br%C3%BAjula-rodea-aviones-de-combate-aviones-comerciales-y-aviones-de-pist%C3%B3n.jpg",
             id: uuidv4(),
             color: ["#F24933", "#F9B437"],
             audio: "https://s1-bos.liveatc.net/lszh1_apr_s?nocache=2023033018281146619",
@@ -15,7 +15,7 @@ function chillHop() {
         {   
             name:"CYQM Gnd/Twr/Misc - Moncton",
             artist: "New Brunswick, Canada",
-            cover: "https://cdn.xxl.thumbs.canstockphoto.com/illustration-of-radar-vector-clipart_csp5212180.jpg",
+            cover: "https://us.123rf.com/450wm/micelinjo/micelinjo1406/micelinjo140600004/28995816-pantalla-de-radar-con-br%C3%BAjula-rodea-aviones-de-combate-aviones-comerciales-y-aviones-de-pist%C3%B3n.jpg",
             id: uuidv4(),
             color: ["#D7B0D5", "#CBDFE0"],
             audio: "https://s1-fmt2.liveatc.net/cyqm_gnd_twr?nocache=2023033018292017188",
@@ -25,7 +25,7 @@ function chillHop() {
             
             name:"RJTT Tokyo Control",
             artist: "Tokyo, Japan",
-            cover: "https://cdn.xxl.thumbs.canstockphoto.com/illustration-of-radar-vector-clipart_csp5212180.jpg",
+            cover: "https://us.123rf.com/450wm/micelinjo/micelinjo1406/micelinjo140600004/28995816-pantalla-de-radar-con-br%C3%BAjula-rodea-aviones-de-combate-aviones-comerciales-y-aviones-de-pist%C3%B3n.jpg",
             id: uuidv4(),
             color: ["#D7B0D5", "#CBDFE0"],
             audio: "https://s1-bos.liveatc.net/rjtt_control?nocache=2023033018302892205",
@@ -34,7 +34,7 @@ function chillHop() {
         {
             name:"VHHH App/Dep/Dir/Zone",
             artist: "Hong Kong, Hong Kong",
-            cover: "https://cdn.xxl.thumbs.canstockphoto.com/illustration-of-radar-vector-clipart_csp5212180.jpg",
+            cover: "https://us.123rf.com/450wm/micelinjo/micelinjo1406/micelinjo140600004/28995816-pantalla-de-radar-con-br%C3%BAjula-rodea-aviones-de-combate-aviones-comerciales-y-aviones-de-pist%C3%B3n.jpg",
             id: uuidv4(),
             color: ["#1D2731", "#F17E76"],
             audio: "https://s1-bos.liveatc.net/vhhh5?nocache=2023033018330240734",
@@ -43,7 +43,7 @@ function chillHop() {
         {
             name:"SBPB Info - Parnaiba",
             artist: "Brazil",
-            cover: "https://cdn.xxl.thumbs.canstockphoto.com/illustration-of-radar-vector-clipart_csp5212180.jpg",
+            cover: "https://us.123rf.com/450wm/micelinjo/micelinjo1406/micelinjo140600004/28995816-pantalla-de-radar-con-br%C3%BAjula-rodea-aviones-de-combate-aviones-comerciales-y-aviones-de-pist%C3%B3n.jpg",
             id: uuidv4(),
             color: ["#008781", "#cfc08c"],
             audio: "https://s1-fmt2.liveatc.net/sbpb2?nocache=2023033018341212402",
@@ -52,7 +52,7 @@ function chillHop() {
         {
             name:"MMMX Tower",
             artist: "Mexico City, Mexico",
-            cover: "https://cdn.xxl.thumbs.canstockphoto.com/illustration-of-radar-vector-clipart_csp5212180.jpg",
+            cover: "https://us.123rf.com/450wm/micelinjo/micelinjo1406/micelinjo140600004/28995816-pantalla-de-radar-con-br%C3%BAjula-rodea-aviones-de-combate-aviones-comerciales-y-aviones-de-pist%C3%B3n.jpg",
             id: uuidv4(),
             color: ["#845648","#FDFBDF"],
             audio: "https://s1-fmt2.liveatc.net/mmmx1_twr?nocache=2023033018371712084",
@@ -61,7 +61,7 @@ function chillHop() {
         {
             name:"UUEE Twr/App/Radar",
             artist: "Moscow, Russia",
-            cover: "https://cdn.xxl.thumbs.canstockphoto.com/illustration-of-radar-vector-clipart_csp5212180.jpg",
+            cover: "https://us.123rf.com/450wm/micelinjo/micelinjo1406/micelinjo140600004/28995816-pantalla-de-radar-con-br%C3%BAjula-rodea-aviones-de-combate-aviones-comerciales-y-aviones-de-pist%C3%B3n.jpg",
             id: uuidv4(),
             color: ["#578CCE", "#E9D6D0"],
             audio: "https://s1-fmt2.liveatc.net/uuee3?nocache=2023033018350465337",
@@ -70,7 +70,7 @@ function chillHop() {
         {
             name:"CYVR App/Dep #1 ",
             artist: "Vancouver, British Columbia, Canada",
-            cover: "https://cdn.xxl.thumbs.canstockphoto.com/illustration-of-radar-vector-clipart_csp5212180.jpg",
+            cover: "https://us.123rf.com/450wm/micelinjo/micelinjo1406/micelinjo140600004/28995816-pantalla-de-radar-con-br%C3%BAjula-rodea-aviones-de-combate-aviones-comerciales-y-aviones-de-pist%C3%B3n.jpg",
             id: uuidv4(),
             color: ["#7BC8DD", "#D64A59"],
             audio: "https://s1-bos.liveatc.net/cyvr1_app?nocache=2023033018360014111",
