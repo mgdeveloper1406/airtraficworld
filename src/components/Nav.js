df0e3534ab57207037508704581b3ca909382146
MG_Apps <99666081+mgdeveloper1406@users.noreply.github.com>
2023-03-30 15:39:55 -0300

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,12 +9,12 @@ const Nav = ({ setLibraryStatus, libraryStatus }) => {
   
     return (
       <nav>
-        <h1>Lección de esta semana</h1>
+        <h1>International Feeds</h1>
         <button
           className={libraryStatus ? "library-active" : ""}
           onClick={openLibraryHandler}
         >
-          Lecciones
+          + International Feeds
           <FontAwesomeIcon icon={faMusic}></FontAwesomeIcon>
         </button>
       </nav>
